772ba2976651ca1a9ed8fc5d8a7099f46812d2a6
Salzi4444 <84124037+Salzi4444@users.noreply.github.com>
2023-11-02 13:16:53 +0100

    corrected profile removed autotiling installation

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 sudo apt install sway waybar swayidle swaylock rofi alacritty xwayland playerctl brightnessctl python3-pip neofetch nextcloud-desktop wget network-manager curl gpg -y
 sudo apt remove foot nano -y
 sudo apt autoremove -y
-
-pip3 install autotiling
-sudo mv ~/.local/bin/autotiling /usr/local/bin/autotiling
-
